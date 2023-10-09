1b1970eda9476e2dc1d1ce72eced8b00eded8036
d-kingangi <itsronduncan@gmail.com>
2023-10-09 11:47:19 +0300

    one more query

---
+++ BRC20-TXs.sql
@@ -0,0 +1,22 @@
+SELECT 
+    /* rename the block_date column as DateTime*/
+    block_date as DateTime,
+    /* count the no. of rows as Number_of_Inscriptions*/
+    Count(*) as Number_of_Inscription ,
+    /* Calculate sum of output_value column, represent the total as OutputBTC*/
+    sum(output_value) as OutputBTC, 
+    /* find sum of total fee as Fee*/
+    sum(fee) as Fee
+FROM bitcoin.transactions
+WHERE 
+    1 = 1  
+    and 
+    /* for blocks greater than height 779831*/
+    block_height > 779831
+AND 
+    /* filter transactions where hex column has specified the hex patterns. The % acts as wildcards, to match characters before/after the patterns*/
+    cast(hex AS VARCHAR) LIKE '%0063036f726401%'
+AND 
+    cast(hex AS VARCHAR) LIKE '%6272632d3230%'
+GROUP BY 1
+ORDER BY 1 DESC
