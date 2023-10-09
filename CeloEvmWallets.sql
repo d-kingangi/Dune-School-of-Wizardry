bf665145c4abb90bac6e0b05af1161cdd07d8a46
d-kingangi <itsronduncan@gmail.com>
2023-10-09 11:02:42 +0300

    1 more query

---
+++ CeloEvmWallets.sql
@@ -0,0 +1,16 @@
+select
+    /* truncate time interval, blocktime into  date_trunc*/
+    date_trunc('{{Time interval}}', block_time) as time,
+    /* find unique sender and receiver addresses */
+    count(distinct "from") unique_senders_addresses,
+    count(distinct "to") unique_receivers_addresses
+from celo.transactions
+/* for succesful transactions*/
+where success = true
+/*  block_time column in the celo.transactions table i.e transction timestamp.
+    Truncate the current timestamp, now(), to specified time interval i.e {{Day}} 
+    subtract a time interval from the truncated timestamp. In this case, i.e {{720}} days.
+    */
+and block_time >= DATE_TRUNC('{{Day}}',now()) - interval '{{720}}' DAY
+group by 1 
+order by 1 desc
