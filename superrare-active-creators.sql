a37b02afb3e38d768e7122d64ac2fd1f4332c785
d-kingangi <itsronduncan@gmail.com>
2023-11-07 13:59:06 +0300

    taproot

---
+++ superrare-active-creators.sql
@@ -0,0 +1,16 @@
+--query to find active creators on superrare
+--superrare: nft marketplace
+
+WITH artists AS
+  (SELECT _creator AS artist
+   FROM superrare_ethereum.SuperRare_evt_WhitelistCreator
+   UNION ALL 
+   SELECT _newAddress
+   FROM xcopy_ethereum.superrare_asset_v2_evt_AddToWhitelist)
+
+SELECT date_trunc('month', block_time) AS MONTH,
+       sum(amount_usd) AS usd
+FROM nft.trades
+WHERE project = 'superrare'
+GROUP BY 1
+ORDER BY 1;
