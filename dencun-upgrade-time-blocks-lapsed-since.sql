29a9be3c24d4d6e0f2232e0f6e68ed445fb24984
d-kingangi <itsronduncan@gmail.com>
2024-03-24 14:09:33 +0300

    Dencun upgrade

---
+++ dencun-upgrade-time-blocks-lapsed-since.sql
@@ -0,0 +1,20 @@
+-- time anb blocks lapsed since the Dencun upgrade - Ethereum layer 1
+
+SELECT
+  (8626176 - MAX(slot)) * -1 
+  AS 
+  blocks_since_Dencun,
+  (TRY_CAST
+  ('2024-03-13 13:55:35' AS TIMESTAMP) 
+  - MAX(time)
+  ) * -1 
+  AS 
+  time_since_Dencun
+FROM beacon.blocks
+
+
+-- blocks_since_Dencun
+-- 78272
+
+-- time_since_Dencun
+-- 10 20:54:24.000
