33eda09400695419691a93a732dec0aa4217abf6
d-kingangi <itsronduncan@gmail.com>
2024-04-15 10:47:25 +0300

    l2

--- l2-total-value-locked-anaylsis.sql
+++ l2-total-value-locked-anaylsis.sql
@@ -1,2 +1,61 @@
 --find the total value locked for the major layer 2 networks
---Celo, Polygon, optimism, arbitrum, base
+-- Polygon, optimism, arbitrum, base
+
+
+SELECT
+  week_start,
+  SUM(TRY_CAST(total_value_locked_base AS DECIMAL)) AS total_value_locked_base,
+  SUM(TRY_CAST(total_value_locked_polygon AS DECIMAL)) AS total_value_locked_polygon,
+  SUM(TRY_CAST(total_value_locked_arbitrum AS DECIMAL)) AS total_value_locked_arbitrum,
+  SUM(TRY_CAST(total_value_locked_optimism AS DECIMAL)) AS total_value_locked_optimism
+FROM (
+  SELECT
+    DATE_TRUNC('week', block_time) AS week_start,
+    SUM(TRY_CAST(value AS DECIMAL)) AS total_value_locked_base,
+    0 AS total_value_locked_polygon,
+    0 AS total_value_locked_arbitrum,
+    0 AS total_value_locked_optimism
+  FROM base.transactions
+  WHERE
+    block_time >= TRY_CAST('2023-01-01' AS DATE)
+  GROUP BY
+    DATE_TRUNC('week', block_time)
+  UNION ALL
+  SELECT
+    DATE_TRUNC('week', block_time) AS week_start,
+    0 AS total_value_locked_base,
+    SUM(TRY_CAST(value AS DECIMAL)) AS total_value_locked_polygon,
+    0 AS total_value_locked_arbitrum,
+    0 AS total_value_locked_optimism
+  FROM polygon.transactions
+  WHERE
+    block_time >= TRY_CAST('2023-01-01' AS DATE)
+  GROUP BY
+    DATE_TRUNC('week', block_time)
+  UNION ALL
+  SELECT
+    DATE_TRUNC('week', block_time) AS week_start,
+    0 AS total_value_locked_base,
+    0 AS total_value_locked_polygon,
+    SUM(TRY_CAST(value AS DECIMAL)) AS total_value_locked_arbitrum,
+    0 AS total_value_locked_optimism
+  FROM arbitrum.transactions
+  WHERE
+    block_time >= TRY_CAST('2023-01-01' AS DATE)
+  GROUP BY
+    DATE_TRUNC('week', block_time)
+  UNION ALL
+  SELECT
+    DATE_TRUNC('week', block_time) AS week_start,
+    0 AS total_value_locked_base,
+    0 AS total_value_locked_polygon,
+    0 AS total_value_locked_arbitrum,
+    SUM(TRY_CAST(value AS DECIMAL)) AS total_value_locked_optimism
+  FROM optimism.transactions
+  WHERE
+    block_time >= TRY_CAST('2023-01-01' AS DATE)
+  GROUP BY
+    DATE_TRUNC('week', block_time)
+) AS subquery
+GROUP BY
+  week_start
