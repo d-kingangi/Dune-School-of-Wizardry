248127000bec13994a56c2353487841664e4f89e
d-kingangi <itsronduncan@gmail.com>
2024-05-09 12:21:44 +0300

    pancakeswap

---
+++ pancakeswap-daily-USD_trade-volume.sql
@@ -0,0 +1,65 @@
+
+
+SELECT
+  block_date,
+  SUM(amount_usd) AS daily_total_amount_usd
+FROM
+  (
+    SELECT
+      block_date,
+      amount_usd
+    FROM
+      pancakeswap_v3_bnb.amm_trades
+    UNION ALL
+    SELECT
+      block_date,
+      amount_usd
+    FROM
+      pancakeswap_v3_ethereum.amm_trades
+    UNION ALL
+    SELECT
+      block_date,
+      amount_usd
+    FROM
+      pancakeswap_v3_arbitrum.amm_trades
+    UNION ALL
+    SELECT
+      block_date,
+      amount_usd
+    FROM
+      pancakeswap_v3_base.amm_trades
+    UNION ALL
+    SELECT
+      block_date,
+      amount_usd
+    FROM
+      pancakeswap_v2_bnb.amm_trades
+    UNION ALL
+    SELECT
+      block_date,
+      amount_usd
+    FROM
+      pancakeswap_v2_arbitrum.amm_trades
+    UNION ALL
+    SELECT
+      block_date,
+      amount_usd
+    FROM
+      pancakeswap_v2_ethereum.amm_trades
+    UNION ALL
+    SELECT
+      block_date,
+      amount_usd
+    FROM
+      pancakeswap_v2_base.amm_trades
+    UNION ALL
+    SELECT
+      block_date,
+      amount_usd
+    FROM
+      pancakeswap.trades
+  ) AS combined_trades
+GROUP BY
+  block_date
+ORDER BY
+  block_date;
