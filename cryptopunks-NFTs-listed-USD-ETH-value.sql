e77048c7bb4a4f1fd88a51d2722529f4796c223a
d-kingangi <itsronduncan@gmail.com>
2024-05-12 15:34:02 +0300

    cryptopunks

--- cryptopunks-NFTs-listed-USD-ETH-value.sql
+++ cryptopunks-NFTs-listed-USD-ETH-value.sql
@@ -3,5 +3,10 @@
 -- total_usd_amount 2821394278.381013
 -- total_nft_holders 7018
 
-
+SELECT
+  COUNT(DISTINCT ct.token_id) AS total_nfts_listed,
+  SUM(ct.amount_usd) AS total_eth_amount, 
+  SUM(ct.amount_usd) AS total_usd_amount,
+  COUNT(DISTINCT ct.buyer) AS total_nft_holders 
+FROM cryptopunks.trades AS ct
 
