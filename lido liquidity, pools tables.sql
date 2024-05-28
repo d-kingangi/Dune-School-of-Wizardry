c4dde36a784f4b9f7e2ed2aed1a081bd8504f142
d-kingangi <itsronduncan@gmail.com>
2024-05-28 13:21:46 +0300

    lido liquidity pools

---
+++ lido liquidity, pools tables.sql
@@ -0,0 +1,191 @@
+
+
+SELECT
+  *
+FROM
+  lido.liquidity
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_arbitrum.uniswap_v3_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_ethereum.uniswap_v3_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_optimism.velodrome_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_arbitrum.ramses_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_arbitrum.kyberswap_v2_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_ethereum.liquidity
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_ethereum.kyberswap_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_ethereum.kyberswap_v2_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_ethereum.uniswap_v2_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_ethereum.curve_steth_pool
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_ethereum.curve_steth_ng_pool
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_optimism.curve_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_ethereum.maverick_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_ethereum.balancer_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_polygon.balancer_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_arbitrum.balancer_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_optimism.balancer_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_arbitrum.camelot_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_optimism.uniswap_v3_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_ethereum.curve_wsteth_reth_pool
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_ethereum.curve_steth_frxeth_pool
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_ethereum.curve_steth_conc_pool
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_arbitrum.curve_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_zksync.syncswap_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_base.uniswap_v3_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_optimism.velodrome_v2_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_optimism.kyberswap_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_arbitrum.kyberswap_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_ethereum.solidly_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_ethereum.pancakeswap_v3_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_base.aerodrome
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_base.kyberswap_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_polygon.kyberswap_v2_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_optimism.kyberswap_v2_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_polygon.uniswap_v3_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_liquidity_arbitrum.wombat_pools
+UNION ALL
+SELECT
+  *
+FROM
+  lido_ethereum_liquidity.kyberswap_pools
