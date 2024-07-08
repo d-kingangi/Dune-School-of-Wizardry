94fa1a716f722be2d5613d07f4ce796381d35eb7
d-kingangi <itsronduncan@gmail.com>
2024-07-08 15:07:58 +0300

    $JUP

---
+++ JUP-cumulative-holders.sql
@@ -0,0 +1,29 @@
+
+WITH
+  user_first_ts AS (
+    SELECT
+      ELEMENT_AT(account_keys, 0 + 1) AS wallet,
+      MIN(DATE_TRUNC('day', block_time)) AS first_seen
+    FROM
+      solana.transactions
+    WHERE
+      NOT block_time IS NULL
+      AND CONTAINS(
+        account_keys,
+        'JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN'
+      )
+      AND block_time >= DATE_TRUNC('day', NOW() - INTERVAL '14' day)
+      AND block_time < DATE_TRUNC('day', NOW())
+    GROUP BY
+      1
+  )
+SELECT
+  first_seen AS day,
+  SUM(COUNT(DISTINCT wallet)) OVER (
+    ORDER BY
+      first_seen NULLS FIRST
+  ) AS cumulative_wallets
+FROM
+  user_first_ts
+GROUP BY
+  1
