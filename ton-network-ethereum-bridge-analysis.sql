28f6da579858503324a5faf94ef719dfb5b40d27
d-kingangi <itsronduncan@gmail.com>
2024-07-19 10:10:45 +0300

    TON

---
+++ ton-network-ethereum-bridge-analysis.sql
@@ -0,0 +1,14 @@
+day
+
+total_transfers
+
+SELECT
+  DATE_TRUNC('week', evt_block_time) AS day,
+  COUNT(*) AS total_transfers
+FROM erc20_ethereum.evt_transfer
+WHERE
+  "to" = 0xb323692b6d4DB96af1f52E4499a2bd0Ded9af3C5
+GROUP BY
+  DATE_TRUNC('week', evt_block_time)
+ORDER BY
+  day
